55ec57bcab1d431aed4d57dfc5f45dfedcb406f3
om9022 <omkarchikane607@gmail.com>
2021-07-17 09:45:08 +0530

    Updated : One generic method to find maximum

--- src/com/bridgelabz/findmaximum/FindMaximum.java
+++ src/com/bridgelabz/findmaximum/FindMaximum.java
@@ -14,44 +14,19 @@ public class FindMaximum {
 		String fruit2 = "Banana";
 		String fruit3 = "Peach";
 		
-		findMaximumNumber(number1, number2, number3);
-		findMaximumNumber(doubleNumber1, doubleNumber2, doubleNumber3);
-		findMaximumString(fruit1, fruit2, fruit3);
+		FindMaximum(number1, number2, number3);
+		FindMaximum(doubleNumber1, doubleNumber2, doubleNumber3);
+		FindMaximum(fruit1, fruit2, fruit3);
 	}
 
-	private static void findMaximumString(String fruit1, String fruit2, String fruit3) {
-		// TODO Auto-generated method stub
-		String maxmimumString = fruit1;
-		if(maxmimumString.compareTo(fruit2) < 0) {
-			maxmimumString = fruit2;
+	public static<T extends Comparable<T>> void FindMaximum(T value1, T value2, T value3) {
+		T maximum = value1;
+		if(maximum.compareTo(value2) < 0) {
+			maximum = value2;
 		}
-		if(maxmimumString.compareTo(fruit3) < 0) {
-			maxmimumString = fruit3;
+		if(maximum.compareTo(value3) < 0) {
+			maximum = value3;
 		}
-		System.out.println("Maximum String is : " + maxmimumString);
-	}
-
-	private static void findMaximumNumber(Double doubleNumber1, Double doubleNumber2, Double doubleNumber3) {
-		// TODO Auto-generated method stub
-		Double maximumNumber = doubleNumber1;
-		if(Double.compare(maximumNumber, doubleNumber2) == -1) {
-			maximumNumber = doubleNumber2;
-		}
-		if(Double.compare(maximumNumber, doubleNumber3) == -1) {
-			maximumNumber = doubleNumber3;
-		}
-		System.out.println("Maximum no is :" +maximumNumber);
-	}
-
-	private static void findMaximumNumber(Integer number1, Integer number2, Integer number3) {
-		// TODO Auto-generated method stub
-		Integer maximumNumber = number1;
-		if(Integer.compare(maximumNumber, number2) == -1) {
-			maximumNumber = number2;
-		}
-		if(Integer.compare(maximumNumber, number3) == -1) {
-			maximumNumber = number3;
-		}
-		System.out.println("Maximum no is :" +maximumNumber);
+		System.out.println("The maximum is: " + maximum);
 	}
 }
