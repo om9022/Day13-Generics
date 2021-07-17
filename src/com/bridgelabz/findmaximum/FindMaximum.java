fbc9f2175aa3dd5abc649bdb4d7b77e64b071ee9
om9022 <omkarchikane607@gmail.com>
2021-07-17 10:13:33 +0530

    Updated : Use of Options and sortings

--- src/com/bridgelabz/findmaximum/FindMaximum.java
+++ src/com/bridgelabz/findmaximum/FindMaximum.java
@@ -1,53 +1,82 @@
 package com.bridgelabz.findmaximum;
 
-public class FindMaximum<T extends Comparable<T>> {
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class FindMaximum <T extends Comparable<T>>
+{
 	private T value1;
 	private T value2;
 	private T value3;
+	private T[] valueArray;
 
-	public FindMaximum(T value1, T value2, T value3) {
+	public FindMaximum(T value1, T value2, T value3, T[] valueArray)
+	{
 		super();
 		this.value1 = value1;
 		this.value2 = value2;
 		this.value3 = value3;
+		this.valueArray = valueArray;
 	}
 
-	public void findMax() {
-		findMaximum(this.value1, this.value2, this.value3);
+	public void findMaxUsingCompare() 
+	{
+		findMaximumUsingCompare(this.value1, this.value2, this.value3,this.valueArray);
 	}
 
-	public static<T extends Comparable<T>> void findMaximum(T value1, T value2, T value3) {
-		T maximum = value1;
-		if(maximum.compareTo(value2) < 0) {
-			maximum = value2;
+	public static <T extends Comparable<T>> void findMaximumUsingCompare(T value1, T value2, T value3,T[] valueArray) 
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter by which way you have to fnd maximum \n 1) Using Compaare \n 2) Using Sorting");
+		int option = scanner.nextInt();
+		switch (option) {
+		case 1:
+			T maximum = value1;
+			if (maximum.compareTo(value2) < 0)
+			{
+				maximum = value2;
+			}
+			if (maximum.compareTo(value3) < 0 )
+			{
+				maximum = value3;
+			}
+			System.out.println("The Maximum is "+ maximum);			
+			break;
+		case 2:
+			Arrays.sort(valueArray);
+			System.out.println("The Maximum is " + valueArray[2]);
+			break;
+		default:
+			System.err.println("Wrong input value");
+			break;
 		}
-		if(maximum.compareTo(value3) < 0) {
-			maximum = value3;
-		}
-		System.out.println("The maximum is: " + maximum);
-	}	
+	}
 
+	public static void main(String[] args)
+	{
+		Integer intNumber1 = 300;
+		Integer intNumber2 = 1000;
+		Integer intNumber3 = 800;
+		Integer[] intNumbers = {intNumber1,intNumber2,intNumber3};
 
-	public static void main(String[] args) {
-		Integer intNumber1 = 30;
-		Integer intNumber2 = 10;
-		Integer intNumber3 = 80;
-
-		Double doubleNumber1 = 800.3;
-		Double doubleNumber2 = 800.0;
-		Double doubleNumber3 = 800.90;
+		Double doubleNumber1 = 802.3; 
+		Double doubleNumber2 = 803.912; 
+		Double doubleNumber3 = 802.90; 
+		Double[] doubleNumbers = {doubleNumber1, doubleNumber2,doubleNumber3};
 
 		String fruit1 = "Apple";
 		String fruit2 = "Banana";
 		String fruit3 = "Peach";
+		String[] strFruits = {fruit1,fruit2,fruit3};
 
-		FindMaximum<Integer> integer = new FindMaximum<Integer>(intNumber1,intNumber2,intNumber3);
-		integer.findMax();
-		FindMaximum<Double> floatingNumber = new FindMaximum<Double>(doubleNumber1,doubleNumber2,doubleNumber3);
-		floatingNumber.findMax();
-		FindMaximum<String> stringValue  = new FindMaximum<String>(fruit1,fruit2,fruit3);
-		stringValue.findMax();
+		FindMaximum<Integer> integer = new FindMaximum<Integer>(intNumber1,intNumber2,intNumber3,intNumbers) ;
+		integer.findMaxUsingCompare();
+
+		FindMaximum<Double> floatingNumber = new FindMaximum<Double>(doubleNumber1,doubleNumber2,doubleNumber3,doubleNumbers);
+		floatingNumber.findMaxUsingCompare();
+
+		FindMaximum<String> stringValue  = new FindMaximum<String>(fruit1,fruit2,fruit3,strFruits);
+		stringValue.findMaxUsingCompare();
+
 	}
-
-
 }
