98b85a71b716331759116557390b11d88ab51220
om9022 <omkarchikane607@gmail.com>
2021-07-17 09:18:55 +0530

    Modified : Method to find maximum value among floats

--- src/com/bridgelabz/findmaximum/FindMaximum.java
+++ src/com/bridgelabz/findmaximum/FindMaximum.java
@@ -5,7 +5,25 @@ public class FindMaximum {
 		Integer number1 = 30;
 		Integer number2 = 10;
 		Integer number3 = 80;
-		findMaximumNumber(number1, number2, number3);		
+		
+		Double doubleNumber1 = 800.3;
+		Double doubleNumber2 = 800.0;
+		Double doubleNumber3 = 800.90;
+		
+		findMaximumNumber(number1, number2, number3);
+		findMaximumNumber(doubleNumber1, doubleNumber2, doubleNumber3);
+	}
+
+	private static void findMaximumNumber(Double doubleNumber1, Double doubleNumber2, Double doubleNumber3) {
+		// TODO Auto-generated method stub
+		Double maximumNumber = doubleNumber1;
+		if(Double.compare(maximumNumber, doubleNumber2) == -1) {
+			maximumNumber = doubleNumber2;
+		}
+		if(Double.compare(maximumNumber, doubleNumber3) == -1) {
+			maximumNumber = doubleNumber3;
+		}
+		System.out.println("Maximum no is :" +maximumNumber);
 	}
 
 	private static void findMaximumNumber(Integer number1, Integer number2, Integer number3) {
