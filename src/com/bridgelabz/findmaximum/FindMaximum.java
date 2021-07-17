8994c9641c537efb88097aed752aa3390743a5ba
om9022 <omkarchikane607@gmail.com>
2021-07-17 10:06:04 +0530

    Added : generic class to take 3 variables

--- src/com/bridgelabz/findmaximum/FindMaximum.java
+++ src/com/bridgelabz/findmaximum/FindMaximum.java
@@ -1,25 +1,22 @@
 package com.bridgelabz.findmaximum;
 
-public class FindMaximum {
-	public static void main(String[] args) {
-		Integer number1 = 30;
-		Integer number2 = 10;
-		Integer number3 = 80;
-		
-		Double doubleNumber1 = 800.3;
-		Double doubleNumber2 = 800.0;
-		Double doubleNumber3 = 800.90;
-		
-		String fruit1 = "Apple";
-		String fruit2 = "Banana";
-		String fruit3 = "Peach";
-		
-		FindMaximum(number1, number2, number3);
-		FindMaximum(doubleNumber1, doubleNumber2, doubleNumber3);
-		FindMaximum(fruit1, fruit2, fruit3);
+public class FindMaximum<T extends Comparable<T>> {
+	private T value1;
+	private T value2;
+	private T value3;
+
+	public FindMaximum(T value1, T value2, T value3) {
+		super();
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
 	}
 
-	public static<T extends Comparable<T>> void FindMaximum(T value1, T value2, T value3) {
+	public void findMax() {
+		findMaximum(this.value1, this.value2, this.value3);
+	}
+
+	public static<T extends Comparable<T>> void findMaximum(T value1, T value2, T value3) {
 		T maximum = value1;
 		if(maximum.compareTo(value2) < 0) {
 			maximum = value2;
@@ -28,5 +25,29 @@ public class FindMaximum {
 			maximum = value3;
 		}
 		System.out.println("The maximum is: " + maximum);
+	}	
+
+
+	public static void main(String[] args) {
+		Integer intNumber1 = 30;
+		Integer intNumber2 = 10;
+		Integer intNumber3 = 80;
+
+		Double doubleNumber1 = 800.3;
+		Double doubleNumber2 = 800.0;
+		Double doubleNumber3 = 800.90;
+
+		String fruit1 = "Apple";
+		String fruit2 = "Banana";
+		String fruit3 = "Peach";
+
+		FindMaximum<Integer> integer = new FindMaximum<Integer>(intNumber1,intNumber2,intNumber3);
+		integer.findMax();
+		FindMaximum<Double> floatingNumber = new FindMaximum<Double>(doubleNumber1,doubleNumber2,doubleNumber3);
+		floatingNumber.findMax();
+		FindMaximum<String> stringValue  = new FindMaximum<String>(fruit1,fruit2,fruit3);
+		stringValue.findMax();
 	}
+
+
 }
